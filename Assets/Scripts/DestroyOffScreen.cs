2611431115d559a78e3837e2fea330a0cf21ecdc
Matt Duncan <busman84@gmail.com>
2015-07-29 07:55:14 -0500

    Wormsy Readme and gameplay

---
+++ Assets/Scripts/DestroyOffScreen.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOffScreen : MonoBehaviour {
+	
+	public float offset = 16f;
+	public delegate void OnDestroy();
+	public event OnDestroy DestroyCallback;
+
+	private bool offscreen;
+	private float offscreenX = 0;
+	private Rigidbody2D body2D;
+	
+	void Awake () {
+		body2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		offscreenX = (Screen.width / PixelPerfectCamera.pixelsToUnits) / 2 + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var posX = transform.position.x;
+		var dirX = body2D.velocity.x;
+		
+		if (Mathf.Abs (posX) > offscreenX) {
+			if (dirX < 0 && posX < -offscreenX) {
+				offscreen = true;
+			} else if (dirX > 0 && posX > offscreenX) {
+				offscreen = true;
+			}
+			
+		} else {
+			offscreen = false;
+		}
+		
+		if (offscreen) {
+			OnOutOfBounds ();
+		}
+	}
+	
+	public void OnOutOfBounds(){
+		offscreen = false;
+		GameObjectsUtil.Destroy (gameObject);
+
+		if (DestroyCallback != null) {
+			DestroyCallback();
+		}
+	}
+}
